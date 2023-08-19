56f077f61afad3afaf2cf94474c0998ea0608e09
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-08-19 13:58:01 -0400

    deleting movie elements

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -16,18 +16,33 @@ const updateUI = () => {
 	}
 };
 
-const renderNewMovieElement = (title, imageUrl, rating) => {
+const deleteMovieHandler = (movieId) => {
+	let movieIndex = 0;
+	for (const movie of movies) {
+		if (movie.id === movieId) {
+			break;
+		}
+		movieIndex++;
+	}
+	movies.splice(movieIndex, 1);
+	const listRoot = document.getElementById("movie-list");
+	listRoot.children[movieIndex].remove();
+	// listRoot.removeChild(listRoot.children[movieIndex]);
+};
+
+const renderNewMovieElement = (id, title, imageUrl, rating) => {
 	const newMovieElement = document.createElement("li");
 	newMovieElement.className = "movie-element";
 	newMovieElement.innerHTML = `
     <div class="movie-element__image">
-      <img src="${imageUrl}" alt='${title}'>
+      <img src="${imageUrl}" alt="${title}">
     </div>
     <div class="movie-element__info">
       <h2>${title}</h2>
       <p>${rating}/5 stars</p>
     </div>
   `;
+	newMovieElement.addEventListener("click", deleteMovieHandler.bind(null, id));
 	const listRoot = document.getElementById("movie-list");
 	listRoot.append(newMovieElement);
 };
@@ -69,16 +84,22 @@ const addMovieHandler = () => {
 	}
 
 	const newMovie = {
+		id: Math.random().toString(),
 		title: titleValue,
 		image: imageUrlValue,
 		rating: ratingValue,
 	};
 
 	movies.push(newMovie);
-	console.log(newMovie);
+	console.log(movies);
 	toggleMovieModal();
 	clearMovieInput();
-	renderNewMovieElement(newMovie.title, newMovie.image, newMovie.rating);
+	renderNewMovieElement(
+		newMovie.id,
+		newMovie.title,
+		newMovie.image,
+		newMovie.rating
+	);
 	updateUI();
 };
 
